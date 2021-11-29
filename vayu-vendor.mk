9873ae586d19cf2954a46415bf5635ccaf923c60
johnmart19 <johnivan19999@gmail.com>
2021-11-29 11:58:48 +0200

    vayu: Import VulkanAPI Layers from Google Pixel 4
    Imported from dump flame-user 12 SP1A.211105.002 7743617 release-keys
    
    Signed-off-by: David Setiawan <fryevia@foxmail.com>

--- vayu-vendor.mk
+++ vayu-vendor.mk
@@ -1110,6 +1110,8 @@ PRODUCT_PACKAGES += \
     CneApp \
     IWlanService \
     TimeService \
+    adreno_graphics_driver \
+    gpu_profiling_vulkan_layer \
     com.qualcomm.qti.gpudrivers.msmnile.api30 \
     PowerOffAlarm \
     HotwordEnrollmentOKGoogleHEXAGON \
